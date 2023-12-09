572465362bfb0e82908a30c607fe347a56daf745
jmd <joshua.m.dean@gmail.com>
2023-12-08 17:50:27 -0800

    [tests] add: the first insanly basic tests

---
+++ tests/test_cli.py
@@ -0,0 +1,13 @@
+"""Test the CLI."""
+
+from nwscript_language_server.cli import cli, get_version
+
+
+def test_get_version() -> None:
+    """Test that the get_version function returns a string."""
+    assert isinstance(get_version(), str)
+
+
+def test_cli() -> None:
+    """Test the basic cli behavior."""
+    assert cli
