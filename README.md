293c28be9ceebeffaaf55f27d8895087cd67aaaa
jmd <joshua.m.dean@gmail.com>
2024-10-21 11:41:20 -0700

    add: nwscript-lint

--- README.md
+++ README.md
@@ -4,6 +4,40 @@
 
 These are tools related to the Arclight project
 
+## Installation
+
+Ideally, create and activate a Python venv.
+
+```
+pip install arclight
+```
+
+To uninstall delete the venv created above.
+
 ## [nwscriptd](arclight/nwscriptd/README.md)
 
-A language server for nwscript.
+A language server for nwscript. See the [README](arclight/nwscriptd/README.md).
+
+## nwscript-lint
+
+A wrapper around the script parser and resolver
+
+```
+usage: nwscript-lint [-h] [-I INCLUDE] [--no-install] [--no-user] scripts [scripts ...]
+
+A linter for nwscript.
+
+positional arguments:
+  scripts               List of scripts to lint.
+
+options:
+  -h, --help            show this help message and exit
+  -I INCLUDE, --include INCLUDE
+                        Include path(s).
+  --no-install          Disable loading game install files.
+  --no-user             Disable user-level configuration.
+```
+
+Sample output:
+
+![output](screenshots/nwscript-lint-2024-10-21.png)
---
+++ arclight/nwscript_lint/main.py
@@ -0,0 +1,53 @@
+import rollnw
+import argparse
+import os
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(description="A linter for nwscript.")
+
+    parser.add_argument(
+        '-I', '--include',
+        action='append',
+        help="Include path(s).",
+    )
+
+    parser.add_argument(
+        '--no-install',
+        action='store_true',
+        help="Disable loading game install files.",
+    )
+
+    parser.add_argument(
+        '--no-user',
+        action='store_true',
+        help="Disable user-level configuration.",
+    )
+
+    parser.add_argument(
+        'scripts',
+        metavar='scripts',
+        nargs='+',
+        help="List of scripts to lint.",
+    )
+
+    args = parser.parse_args()
+
+    config = rollnw.kernel.config().options()
+    config.include_install = not args.no_install
+    config.include_user = not args.no_user
+    rollnw.kernel.start(config)
+
+    for file in args.scripts:
+        start_time = time.perf_counter()
+        ctx = rollnw.script.Context(args.include)
+        nss = rollnw.script.Nss(
+            file, ctx, os.path.basename(file) == "nwscript.nss")
+        nss.resolve()
+        elapsed_time = (time.perf_counter() - start_time) * 1000
+        print(f"Processed '{file}' in {elapsed_time:.3f} ms")
+
+
+if __name__ == "__main__":
+    main()
